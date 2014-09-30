a9c37dd24e6fe5ef79f41695f86d20337d5bfad6
Michael Cordingley <Michael.Cordingley@gmail.com>
2014-09-30 15:51:09 -0400

    Create inverse.js

---
+++ inverse.js
@@ -0,0 +1,48 @@
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) define(['exports'], factory);
+    else if (typeof exports === 'object') factory(exports);
+    else factory(root.Kinetoscope = {});
+})(this, function (exports) {
+    'use strict';
+    
+    var Inverse = function() {
+        this._boundCallbacks = {};
+        this._singletonCallbacks = {};
+        this._instantiatedSingletons = {};
+        this._registeredObjects = {};
+    };
+    
+    var proto = Inverse.prototype;
+    
+    proto.make = function(name) {
+        if (this._registeredObjects.hasOwnProperty(name)) {
+            return this._registeredObjects[name];
+        }
+        
+        if (this._singletonCallbacks.hasOwnProperty(name)) {
+            if (!this._instantiatedSingletons.hasOwnProperty(name)) {
+                this._instantiatedSingletons[name] = this._singletonCallbacks[name](this);
+            }
+            
+            return this._instantiatedSingletons[name];
+        }
+        
+        if (this._boundCallbacks.hasOwnProperty(name)) {
+            return this._boundCallbacks[name](this);
+        }
+        
+        return null;
+    };
+    
+    proto.bind = function(name, callback) {
+        this._boundCallbacks[name] = callback;
+    };
+    
+    proto.singleton = function(name, callback) {
+        this._singletonCallbacks[name] = callback;
+    };
+    
+    proto.register = function(name, object) {
+        this._registeredObjects[name] = object;
+    };
+});
